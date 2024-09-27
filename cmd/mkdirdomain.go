e2e4c9e488afb17c69877211f556a3b62f1b24ea
Mik-kom4 <androidmikiya9814@gmail.com>
2024-09-27 12:10:59 +0900

    add: 現在のディレクトリを取得

---
+++ cmd/mkdirdomain.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// mkdirdomainCmd represents the mkdirdomain command
+var mkdirdomainCmd = &cobra.Command{
+	Use:   "mkdirdomain",
+	Short: "A brief description of your command",
+	Run: func(cmd *cobra.Command, args []string) {
+		// 現在のディレクトリを所得
+		currentDir, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(currentDir)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(mkdirdomainCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// mkdirdomainCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// mkdirdomainCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
